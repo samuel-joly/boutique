bb50a6218b7647c5287cd4872844b27db6a5abdf
Bouchakour <amine.bouchakour@laplateforme.io>
2020-04-03 19:31:17 +0200

    ajout fichier page php

---
+++ connexion.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang='en'>
+	<head>
+		<title>Boutique</title>
+		<link rel='stylesheet' href='CSS/stylesheet.css' type='text/css'/>		
+		<link rel="stylesheet" type="text/css" href="CSS/boot.css"/>
+		<meta charset='UTF-8'>
+	</head>
+
+	<body id='inscription-body'>
+		<header> <?php include("header.php"); ?> </header>
+		
+		<main class='flexc just-center align-center'>
+			<h1 id='top-title' class='center'>Log In</h1>
+			<form action='' method='post' id='connect-form' class='user-form'>
+				<div class='input-zone'>
+					<label for='login'>Login :</label> <input type='text' name='login' required/>
+				</div>
+
+				<div class='input-zone'>
+					<label for='password'>Password:</label> <input type='password' name='password' required/>
+				</div>
+
+				<input type='submit' name='connect_submit' value='Connexion'/>
+				<?php if($_GET["error"] ?? "" == "password") { ?>
+					<p class='error'>Wrong password or login</p>
+				<?php } ?>
+			</form>
+		</main>
+	</body>
+</html>
+
+
+<?php
+	if(isset($_SESSION["id"]))
+	{
+		header("location:index.php");
+	}
+
+	$stmt = new PDO("mysql:host=localhost;dbname=boutique","root","");
+
+	if(isset($_POST["connect_submit"]))
+	{
+		$login = htmlspecialchars($_POST["login"]);
+		$password =htmlspecialchars($_POST["password"]);
+		
+		if($login != "" && $password != "")
+		{
+			$usr = $stmt->query("SELECT * FROM users WHERE name = '".$login."'")->fetch(PDO::FETCH_ASSOC);
+			if(!empty($usr))
+			{
+				if(password_verify($password, $usr["password"]))
+				{
+					$_SESSION["id"] = $usr["id"];
+					header("location:index.php");
+				}
+				else
+				{
+					header("location:connexion.php?error=password");
+				}
+			}
+		}
+	}
+?>	
---
+++ product.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html lang='en'>
+	<head>
+		<title>Boutique</title>
+		<link rel='stylesheet' type='text/css' href="CSS/stylesheet.css" />		
+		<link rel="stylesheet" type="text/css" href="CSS/boot.css"/>
+		<meta charset='UTF-8'>
+	</head>
+
+	<body id='product-body'>
+		<header> 
+			<?php include("header.php"); ?> 
+		</header>
+
+		<main>
+			<div id='products-box' class='flexc just-center align-center'>
+			<?php
+				$products = $stmt->query("SELECT *, products.id as id_prod FROM products INNER JOIN agents ON products.id_agent = agents.id INNER JOIN users ON agents.id_user = users.id")->fetchAll(PDO::FETCH_ASSOC);
+				//var_dump($products);
+				
+				foreach($products as $product)
+				{ ?>
+					<div class='flexr just-between product-zone'>
+						<div class='product-box flexr just-between align-center center' 
+							style='background-image:url(<?=$product["image"]?>); background-size:cover;'> 
+								<div class=' product-info-product flexr just-between align-center'>
+									<div class='flexc just-center'>
+										<span class='flexr just-start'>
+											<h1 class='product-name'><?=$product["title"]?> -- </h1>
+											<h1 class='product-price'><?=$product["price"]?>$</h1>
+										</span>
+										<p>Superficie: <?=$product["size"]?> m&#178;</p>
+										<p>Agency : <?=$product["cost"]?> $</p>
+									</div>
+
+									<div class='product-agent-zone flexc just-center align-center'>
+										<h1 class='product-agent-name'><?=$product["name"]?></h1>
+										<img src='Media/Images/Avatars/<?=$product["avatar"]?>' class='product-agent-avatar'/>
+									</div>
+								</div>
+
+							</div>
+							<div class='product-infos-plus flexc just-evenly align-start'>
+								<p>Size: <?=$product["size"]?>m&#178;</p>
+								<p>Location: <?=$product["location"]?></p>
+								<p>Orientation: <?=$product["orientation"]?></p>
+								<p>Staff: <?=$product["staff"]?></p>
+								<p>Cost/Year: <?=$product["cost"]?>$</p>
+								<a href='product.php?id=<?=$product["id_prod"]?>' class='product-link'>See More</a>
+							</div>
+						</div>
+				<?php	}	?>
+			</div>	
+		</main>
+
+		<footer>
+			<?php include("footer.php"); ?>
+		</footer>
+	
+	</body>
+</html>
