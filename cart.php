992221dc6191886c83451c67f4548a475edaa5db
Lastscream53 <55872979+amine-bouchakour@users.noreply.github.com>
2020-04-14 22:05:04 +0200

    Add files via upload
    suppression div animation sur photo article (page panier)

--- cart.php
+++ cart.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -10,46 +12,80 @@
 
     <body id="cart-body">
 
+    
+
         <header> 
-            <?php include("header.php"); ?> 
+            <?php include("header.php"); 
+            
+            ?> 
         </header>   
+
+<?php
+                if(isset($_POST['removeArticle'])){
+                    $id_product=$_POST['id_product'];
+                    $connexion=mysqli_connect("localhost","root","","boutique");
+                    $requete="DELETE FROM basket WHERE id_product='".$id_product."' AND id_user='".$_SESSION['id']."' ";
+                    // echo $requete;
+                    $query=mysqli_query($connexion,$requete);
+                    header("location:cart.php");
+                    // echo $requete;
+                    //echo $product['id_basket']."</br></br>";
+                }
+            
+?>
 
 
         <main class='flexr just-center align-center'>
 
             <section id="panierList"  class='flexc just-between align-center'>
             
+            <div id='products-box-basket' class='flexc just-between align-center'>
+
+                    <?php
+    					$query = "SELECT title, image, users.name, users.avatar as avatar,  products.id as id_prod, size, price, cost , staff, location, orientation, basket.id as id_basket FROM products
+						INNER JOIN agents ON products.id_agent = agents.id
+						INNER JOIN users ON agents.id_user = users.id
+                        INNER JOIN basket ON products.id = basket.id_product
+						WHERE basket.id_user='".$_SESSION['id']."'"
+                        ;
+
+					
+				$products = $stmt->query($query)->fetchAll(PDO::FETCH_ASSOC); 
+				foreach($products as $product)
+				{ ?>
+					<div class='flexr just-between product-zone'>
+						<div class='product-box flexr just-between align-center center' 
+							style='background-image:url(<?=$product["image"]?>); background-size:cover;'> 
+								<div class='lexr just-between align-center'>
+							
+
+									<div class='product-agent-zone flexc just-center align-center'>
+									
+									</div>
+								</div>
+
+							</div>
+							<div class='product-infos-basket flexc just-evenly align-start'>
+								<p>Size: <?=$product["size"]?>m&#178;</p>
+								<p>Location: <?=$product["location"]?></p>
+								<p>Orientation: <?=$product["orientation"]?></p>
+								<p>Staff: <?=$product["staff"]?></p>
+                                <p>Cost/Year: <?=$product["cost"]?>$</p>
+                                <form method="POST">
+                                    <input type="hidden" name="id_product" value="<?php echo $product['id_prod'] ;?>">
+                                    <input type="submit" name="removeArticle" id="removeArticle" value="Remove">
+                                </form>
+							</div>
+						</div>
                 <?php
-                $connexion= mysqli_connect("localhost","root","","boutique");
-                $requete=" SELECT * FROM basket AS PAN INNER JOIN products AS PRO On PRO.id=PAN.id_product WHERE id_user='".$_SESSION['id']."'";
-                $query=mysqli_query($connexion,$requete);
-                $resultat=mysqli_fetch_all($query);
-
-                for($i=0;$i<count($resultat);++$i){
-
-                    ?>
-                        <div id="houseInBasket">
-                    <?php
-
-                    echo $idProductBasket=$resultat[$i][0].'</br>';
-                    echo $idProduct=$resultat[$i][2].'</br>';
-                    echo $quantity=$resultat[$i][3].'</br>';
-                    echo $price=$resultat[$i][5].'</br>';
-                    echo $title=$resultat[$i][6].'</br>';
-                    echo $img=$resultat[$i][8].'</br>';
-                    echo $size=$resultat[$i][9].'</br>';
-                    echo $location=$resultat[$i][10].'</br>';
-                    echo $orientation=$resultat[$i][11].'</br>';
-                    echo $staff=$resultat[$i][12].'</br>';
-                    echo $cost=$resultat[$i][13].'</br>';
-                    echo $idAgent=$resultat[$i][14].'</br>';
-                    echo $maxQuantity=$resultat[$i][15].'</br>';
-
-                    ?>
-                        </div>
-                    <?php
+                            
+                    
                 }
-                ?>
+
+
+            
+            ?>
+			</div>	
 
             </section>
 
@@ -67,8 +103,8 @@
                     Les options proposées sur le/les biens <br>
                     <div id="optionDiv">
                         Option 1 :
-                        <label>Oui</label><input type="radio"></input>
-                        <label>Non</label><input type="radio"></input><br>
+                        <label for="ouiOption1">Oui</label><input type="radio">
+                        <label for="nonOption1">Non</label><input type="radio"><br>
                     </div>
                     <div id="optionDiv">
                         Option 2 :
